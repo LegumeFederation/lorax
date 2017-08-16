6404e87c2222afbce7578db83417b582813d7dc9
Joel Berendzen <joel@generisbio.com>
2017-08-16 13:00:40 -0600

    more bugs in test script

--- build_scripts/run_lorax_tests.sh
+++ build_scripts/run_lorax_tests.sh
@@ -18,26 +18,29 @@ ${root}/bin/lorax_env supervisord
 # Sleep for a while, then get the statuses.
 #
 sleep 10
-${root}/lorax_env supervisorctl status
+${root}/bin/orax_env supervisorctl status
 echo "Hopefully you just saw a set of processes with status RUNNING!"
-sleep 10
+sleep 5
 #
 # Create the test directory and cd to it.
 #
 rm -rf test_lorax
 rm -f ~/.lorax/lorax_rc
+mkdir test_lorax
 pushd test_lorax
-${root}/bin/lorax_env lorax_create_test_files
+echo "Getting a set of test files."
+${root}/bin/lorax_env lorax create_test_files
 echo "Running test of lorax server."
 ./lorax_test.sh
+echo ""
 echo "Hopefully you just got the message  \"lorax tests completed successfully\" ."
-sleep 10
+sleep 5
 popd
 #
 # Clean up.
 #
 echo "Stopping lorax processes."
-${root}/bin/lorax_env supervisorctl stop
+${root}/bin/lorax_env supervisorctl shutdown
 rm -r test_lorax
 echo "Tests completed successfully."
 trap - EXIT
