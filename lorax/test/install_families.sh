6caf69a4a6fe7e7d8a105dfdc60d70f173146855
Joel Berendzen <joel@generisbio.com>
2017-11-22 12:20:02 -0700

    multi-family loading

---
+++ lorax/test/install_families.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# This script downloads and installs gene familes into lorax.
+#
+phytozome="phytozome_10_2"
+legfed="legfed_v1_0"
+set -e
+install_family() {
+	echo "Downloading $1 families"
+        curl -o ${1}.tar.gz http://data.comparative-legumes.org/gene_families/${1}.tar.gz
+	echo "Unpacking $1 families"
+	tar xzf ${1}.tar.gz
+	echo "Creating $1 gene families"
+	./load_family.sh  ${1}/fasta/ ${1}/hmm/ $1
+	rm -rf ${1}/ ${1}.tar.gz
+}
+read -r -p "Download/install $phytozome gene families (900 MB)? [y/N] " phytozome_response
+read -r -p "Download/install $legfed  gene families (700 MB)? [y/N] " legfed_response
+case "$phytozome_response" in
+    [yY][eE][sS]|[yY]) 
+        install_family $phytozome
+        echo ""
+        ;;
+    *)
+        echo "Skipping $phytozome install."
+        ;;
+esac
+case "$legfed_response" in
+    [yY][eE][sS]|[yY])  
+        install_family $legfed
+        ;;
+    *)
+        echo "Skipping $legfed install."
+        ;;
+esac
+echo "Installation of families done."
--- lorax/test/load_family.sh
+++ lorax/test/load_family.sh
@@ -10,15 +10,14 @@ FASTA files and HMM's.  The HMM's may be in different directories,
 but the file stems must match.
 
 Usage:
-	./create_families.sh [-v]|[-q] fastapath hmmpath
+	./load_family.sh [-v]|[-q] fastapath hmmpath familyname
 
 Flags:
 	-v  verbose, output family name for each file created
 	-q  quiet, do not display progress bar
 
 Example:
-	./create_families.sh -v legume_gene_families_phytozome_10_2/fasta/ \
-		legume_gene_families_phytozome_10_2/hmm/
+	./load_family.sh -v phytozome_10_2/fasta/ phytozome_10_2/hmm/ phytozome_10_2
 """
 #
 function ProgressBar {
@@ -39,18 +38,18 @@ function PostPut {
         nchars=$(grep -v \> ${seqfile} | wc -c)
         avg_len=$(echo "$nchars $nseqs" | awk '{print int($1/$2+0.5)}')
         if [ "$_V" -ne 0 ]; then
-            echo "Creating family ${fam} with ${nseqs} sequences of length ${avg_len}."
+            echo "Loading family ${fam} with ${nseqs} sequences of length ${avg_len}."
         elif [ "$_Q" -eq 0 ]; then
             ProgressBar ${count} ${nfiles}
         fi
         ./post_FASTA.sh peptide ${seqfile} ${fam} sequences
         if [ $? -ne 0 ]; then
-            >&2 echo "POST of FASTA failed on ${fam}"
+            >&2 echo "POST of FASTA failed on ${fam}."
  		    exit 1
         fi
  	    ./put_HMM.sh ${hmmpath}/${fam}.hmm ${fam}
  	    if [ $? -ne 0 ]; then
- 		    >&2 echo "PUT of HMM failed on ${fam}"
+ 		    >&2 echo "PUT of HMM failed on ${fam}."
  		    exit 1
  	    fi
         echo -e "${fam}\t${nseqs}\t${avg_len}">>families.raw
@@ -68,18 +67,18 @@ elif [ "$1" == "-q" ]; then
 	_Q=1
 	shift 1
 fi
-if [ "$#" -lt 2   ]; then
+if [ "$#" -lt 3   ]; then
 	>&2 echo "$DOC"
 	exit 1
 fi
 fastapath=$1
 hmmpath=$2
+familyname=$3
 #
 # Loop over FASTA files, POST FASTA and PUT HMM.
 #
 nfiles=$(find ${fastapath} -type f | wc -l)
-rm -f families.raw
-echo -e "#family_name\tseqs\tavg_len" >families.tsv
+echo -e "#family\tseqs\tavg_len" >${familyname}-families.tsv
 #
 # Post sequences and put HMM's to server.
 #
@@ -87,20 +86,22 @@ find ${fastapath} -name \*.faa |  PostPut
 if [ $? -ne 0 ]; then
     exit 1
 fi
-if [ "$_Q" -ne 0 ]; then
+if [ "$_Q" -eq 0 ]; then
   echo "" # newline after progress bar
 fi
 #
 # Sort, histogram, and print stats on families.
 #
-echo "${nfiles} families created."
-sort -rgk2 families.raw >> families.tsv
-echo "Size-ordered (large to small) list of families can be found in families.tsv"
-rm -f families_histogram.tsv
-echo "Histogram of family sizes can be found in families_histogram.tsv"
-echo -e "#size\tfamilies" >families_histogram.tsv
-grep -v \# families.tsv | awk '{n[$2]++} END {for (i in n) print i,"\t",n[i]}' | sort -n >>families_histogram.tsv
-IFS=',' read -r -a bigfam <<< $(grep -v \# families.tsv | head -1| tr "\\t" ",")
-echo "Largest family is ${bigfam[0]} with ${bigfam[1]} members of average length ${bigfam[2]}."
-IFS=',' read -r -a modal <<< $(grep -v \# families_histogram.tsv | sort -rgk 2 | head -1 | tr "\\t" ",")
-echo "Modal size of families is ${modal[0]} sequences, with ${modal[1]} examples."
+echo "${nfiles} $familyname families created."
+sort -rgk2 families.raw >> ${familyname}-families.tsv
+echo "List of $familyname families can be found in ${familyname}-families.tsv"
+rm -f families.raw ${familyname}-histogram.tsv
+echo "Histogram of $familyname family sizes can be found in ${familyname}-histogram.tsv"
+echo -e "#size\tfamilies" >${familyname}-histogram.tsv
+grep -v \# ${familyname}-families.tsv | \
+  awk '{n[$2]++} END {for (i in n) print i,"\t",n[i]}' | \
+  sort -n >>${familyname}-histogram.tsv
+IFS=',' read -r -a bigfam <<< $(grep -v \# ${familyname}-families.tsv | head -1| tr "\\t" ",")
+echo "Largest $familyname family is ${bigfam[0]} with ${bigfam[1]} members of average length ${bigfam[2]}."
+IFS=',' read -r -a modal <<< $(grep -v \# ${familyname}-histogram.tsv | sort -rgk 2 | head -1 | tr "\\t" ",")
+echo "Modal size of $familyname families is ${modal[0]} sequences, with ${modal[1]} examples."
