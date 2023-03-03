a6d53e84e5cfaf99d86d47aee8b2093ead41986b
adf-ncgr <adf@ncgr.org>
2023-03-02 21:35:36 -0700

    Update install_families.sh
    use hmmalign_trim version of alignments, which are actually reasonably aligned although not containing the entirety of the sequence data. The hmmalign non-trimmed alignments have gaps but don't actually produce coherent alignments in many cases due to however these were constructed from the original output (not the fault of lorax, just garbage in that it was given)

--- lorax/test/install_families.sh
+++ lorax/test/install_families.sh
@@ -31,10 +31,10 @@ then
   echo "Installing Newick trees and MSAs."
   curl $yuckified_data/legume.genefam.fam1.M65K.trees_ML_rooted.tgz |
     tar -xzf -
-  curl $yuckified_data/legume.genefam.fam1.M65K.hmmalign.tgz |
+  curl $yuckified_data/legume.genefam.fam1.M65K.hmmalign_trim.tgz |
     tar -xzf -
   find legume.genefam.fam1.M65K.trees_ML_rooted -type f -exec mv {} {}.nwk \;
-  find legume.genefam.fam1.M65K.hmmalign -type f -exec mv {} {}.faa \;
+  find legume.genefam.fam1.M65K.hmmalign_trim -type f -exec mv {} {}.faa \;
   ${bindir}/copy_family.sh legume.genefam.fam1.M65K.trees_ML_rooted legume.genefam.fam1.M65K.hmmalign legume.genefam.fam1.M65K
   rm -rf legume.genefam.fam1.M65K.trees_ML_rooted legume.genefam.fam1.M65K.hmmalign
   rm -rf legume.genefam.fam1.M65K
